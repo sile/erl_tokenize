8bf438d8b710f21e0a7ddff5835d63e9c2254c25
Takeru Ohta <phjgt308@gmail.com>
2017-05-23 04:20:05 +0900

    Support unread

--- src/char_reader.rs
+++ src/char_reader.rs
@@ -1,24 +1,27 @@
-use std::iter::Peekable;
-
 use {Result, ErrorKind};
 
 #[derive(Debug)]
-pub struct CharReader<T>
-    where T: Iterator<Item = char>
-{
-    chars: Peekable<T>,
+pub struct CharReader<T> {
+    chars: T,
+    unreads: Vec<char>,
 }
 impl<T> CharReader<T>
     where T: Iterator<Item = char>
 {
     pub fn new(chars: T) -> Self {
-        CharReader { chars: chars.peekable() }
+        CharReader {
+            chars: chars,
+            unreads: Vec::new(),
+        }
+    }
+    pub fn unread(&mut self, c: char) {
+        self.unreads.push(c)
     }
     pub fn read_while<F>(&mut self, f: F) -> String
         where F: Fn(char) -> bool
     {
         let mut buf = String::new();
-        while let Some(&c) = self.chars.peek() {
+        while let Ok(c) = self.peek_char() {
             if !f(c) {
                 break;
             }
@@ -27,18 +30,38 @@ impl<T> CharReader<T>
         }
         buf
     }
+    pub fn read_char_if(&mut self, expects: &str) -> Option<char> {
+        self.peek_char()
+            .ok()
+            .and_then(|c| if expects.contains(c) {
+                          self.consume_char();
+                          Some(c)
+                      } else {
+                          None
+                      })
+    }
     pub fn peek_char(&mut self) -> Result<char> {
-        let c = track_try!(self.chars.peek().cloned().ok_or(ErrorKind::UnexpectedEof));
-        Ok(c)
+        if let Some(c) = self.unreads.last().cloned() {
+            Ok(c)
+        } else if let Some(c) = self.chars.next() {
+            self.unreads.push(c);
+            Ok(c)
+        } else {
+            track_panic!(ErrorKind::UnexpectedEof)
+        }
     }
     pub fn read_char(&mut self) -> Result<char> {
-        let c = track_try!(self.chars.next().ok_or(ErrorKind::UnexpectedEof));
-        Ok(c)
+        if let Some(c) = self.unreads.pop() {
+            Ok(c)
+        } else {
+            let c = track_try!(self.chars.next().ok_or(ErrorKind::UnexpectedEof));
+            Ok(c)
+        }
     }
     pub fn consume_char(&mut self) {
-        let _ = self.chars.next();
+        let _ = self.read_char();
     }
     pub fn is_eos(&mut self) -> bool {
-        self.chars.peek().is_none()
+        self.peek_char().is_err()
     }
 }
