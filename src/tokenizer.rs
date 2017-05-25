85832d9dad873627e099d4f342aec9fc2e45bdb0
Takeru Ohta <phjgt308@gmail.com>
2017-05-26 03:09:23 +0900

    Add getter methods

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -3,27 +3,33 @@ use {Result, Token};
 #[derive(Debug)]
 pub struct Tokenizer<'a> {
     text: &'a str,
-    start: usize,
+    position: usize,
 }
 impl<'a> Tokenizer<'a> {
     pub fn new(text: &'a str) -> Self {
-        Tokenizer { text, start: 0 }
+        Tokenizer { text, position: 0 }
+    }
+    pub fn text(&self) -> &'a str {
+        self.text
+    }
+    pub fn position(&self) -> usize {
+        self.position
     }
 }
 impl<'a> Iterator for Tokenizer<'a> {
     type Item = Result<Token<'a>>;
     fn next(&mut self) -> Option<Self::Item> {
-        if self.start <= self.text.len() {
+        if self.position >= self.text.len() {
             None
         } else {
-            let text = unsafe { self.text.slice_unchecked(self.start, self.text.len()) };
+            let text = unsafe { self.text.slice_unchecked(self.position, self.text.len()) };
             match track!(Token::from_text(text)) {
                 Err(e) => {
-                    self.start = self.text.len();
+                    self.position = self.text.len();
                     Some(Err(e))
                 }
                 Ok(t) => {
-                    self.start += t.text().len();
+                    self.position += t.text().len();
                     Some(Ok(t))
                 }
             }
