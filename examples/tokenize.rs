1f9e5bb127c6420941c8a39050d5c7e6ddb077ee
Takeru Ohta <phjgt308@gmail.com>
2017-06-08 00:13:27 +0900

    Show elapsed time

--- examples/tokenize.rs
+++ examples/tokenize.rs
@@ -5,6 +5,7 @@ extern crate trackable;
 
 use std::fs::File;
 use std::io::Read;
+use std::time::{Duration, Instant};
 use clap::{App, Arg};
 use erl_tokenize::Tokenizer;
 
@@ -20,6 +21,7 @@ fn main() {
     let mut file = File::open(src_file).expect("Cannot open file");
     file.read_to_string(&mut src).expect("Cannot read file");
 
+    let start_time = Instant::now();
     let mut count = 0;
     let tokenizer = Tokenizer::new(&src);
     for result in tokenizer {
@@ -30,4 +32,10 @@ fn main() {
         count += 1;
     }
     println!("TOKEN COUNT: {}", count);
+    println!("ELAPSED: {:?} seconds",
+             to_seconds(Instant::now() - start_time));
+}
+
+fn to_seconds(duration: Duration) -> f64 {
+    duration.as_secs() as f64 + (duration.subsec_nanos() as f64) / 1_000_000_000.0
 }
