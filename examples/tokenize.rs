193a7ba8bcd4024232e2e59ce52bf4be78807b3b
Takeru Ohta <phjgt308@gmail.com>
2017-06-08 00:03:44 +0900

    Add `silent` option for benchmarkings

--- examples/tokenize.rs
+++ examples/tokenize.rs
@@ -11,16 +11,23 @@ use erl_tokenize::Tokenizer;
 fn main() {
     let matches = App::new("tokenize")
         .arg(Arg::with_name("SOURCE_FILE").index(1).required(true))
+        .arg(Arg::with_name("SILENT").long("silent"))
         .get_matches();
     let src_file = matches.value_of("SOURCE_FILE").unwrap();
+    let silent = matches.is_present("SILENT");
 
     let mut src = String::new();
     let mut file = File::open(src_file).expect("Cannot open file");
     file.read_to_string(&mut src).expect("Cannot read file");
 
+    let mut count = 0;
     let tokenizer = Tokenizer::new(&src);
     for result in tokenizer {
         let (token, pos) = track_try_unwrap!(result);
-        println!("[pos:{:?}] {:?}", pos, token);
+        if !silent {
+            println!("[pos:{:?}] {:?}", pos, token);
+        }
+        count += 1;
     }
+    println!("TOKEN COUNT: {}", count);
 }
