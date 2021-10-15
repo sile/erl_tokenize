f79bdd3e28f881c8df985a9ba50f1fe467b5670c
Takeru Ohta <phjgt308@gmail.com>
2021-10-15 20:42:06 +0900

    Use structopt

--- examples/tokenize.rs
+++ examples/tokenize.rs
@@ -1,22 +1,25 @@
 #[macro_use]
 extern crate trackable;
 
-use clap::{App, Arg};
 use erl_tokenize::{PositionRange, Tokenizer};
 use std::fs::File;
 use std::io::Read;
 use std::time::{Duration, Instant};
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(rename_all = "kebab-case")]
+struct Opt {
+    src_file: String,
+    #[structopt(long)]
+    silent: bool,
+}
 
 fn main() {
-    let matches = App::new("tokenize")
-        .arg(Arg::with_name("SOURCE_FILE").index(1).required(true))
-        .arg(Arg::with_name("SILENT").long("silent"))
-        .get_matches();
-    let src_file = matches.value_of("SOURCE_FILE").unwrap();
-    let silent = matches.is_present("SILENT");
+    let opt = Opt::from_args();
 
     let mut src = String::new();
-    let mut file = File::open(src_file).expect("Cannot open file");
+    let mut file = File::open(opt.src_file).expect("Cannot open file");
     file.read_to_string(&mut src).expect("Cannot read file");
 
     let start_time = Instant::now();
@@ -24,7 +27,7 @@ fn main() {
     let tokenizer = Tokenizer::new(&src);
     for result in tokenizer {
         let token = track_try_unwrap!(result);
-        if !silent {
+        if !opt.silent {
             println!("[{:?}] {:?}", token.start_position(), token.text());
         }
         count += 1;
