850184c70c70176d0f04c7dd3f8aef52e3e080cc
Takeru Ohta <phjgt308@gmail.com>
2025-11-12 22:59:19 +0900

    chore: replace clap with noargs for argument parsing

--- examples/tokenize.rs
+++ examples/tokenize.rs
@@ -1,30 +1,38 @@
-use clap::Parser;
-use erl_tokenize::{PositionRange, Tokenizer};
-use orfail::OrFail;
 use std::fs::File;
 use std::io::Read;
 use std::time::{Duration, Instant};
 
-#[derive(Parser)]
-struct Opt {
-    src_file: String,
-    #[clap(long)]
-    silent: bool,
-}
+use erl_tokenize::{PositionRange, Tokenizer};
 
-fn main() -> orfail::Result<()> {
-    let opt = Opt::parse();
+fn main() -> noargs::Result<()> {
+    let mut args = noargs::raw_args();
+    args.metadata_mut().app_name = env!("CARGO_PKG_NAME");
+    args.metadata_mut().app_description = "Tokenize Erlang source code";
+    noargs::HELP_FLAG.take_help(&mut args);
+
+    let silent: bool = noargs::flag("silent")
+        .doc("Suppress token output")
+        .take(&mut args)
+        .is_present();
+    let src_file: String = noargs::arg("<SRC_FILE>")
+        .doc("Source file to tokenize")
+        .take(&mut args)
+        .then(|a| a.value().parse())?;
+    if let Some(help) = args.finish()? {
+        print!("{help}");
+        return Ok(());
+    }
 
     let mut src = String::new();
-    let mut file = File::open(opt.src_file).expect("Cannot open file");
+    let mut file = File::open(src_file).expect("Cannot open file");
     file.read_to_string(&mut src).expect("Cannot read file");
 
     let start_time = Instant::now();
     let mut count = 0;
     let tokenizer = Tokenizer::new(&src);
     for result in tokenizer {
-        let token = result.or_fail()?;
-        if !opt.silent {
+        let token = result?;
+        if !silent {
             println!("[{:?}] {:?}", token.start_position(), token.text());
         }
         count += 1;
